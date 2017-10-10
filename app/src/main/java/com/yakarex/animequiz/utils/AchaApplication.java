1faff1b0e1d3368b659732218371cc6790454df8
AxelRomero <axel@iamat.com>
2017-10-10 17:15:55 -0300

    Added Crashlytics

--- app/src/main/java/com/yakarex/animequiz/utils/AchaApplication.java
+++ app/src/main/java/com/yakarex/animequiz/utils/AchaApplication.java
@@ -2,10 +2,12 @@ package com.yakarex.animequiz.utils;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.ads.MobileAds;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.yakarex.animequiz.R;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by aromero on 28/01/16.
@@ -15,6 +17,7 @@ public class AchaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
                 .build();
